e7b128307edc6b3a9300b059a6a76d3b91756385
João Tozzi <joaotozzi@gmail.com>
2022-10-22 14:44:57 -0300

    usando @ConfigurationProperties

--- src/main/java/br/com/joaotozzi/estudandospringboot/controller/GreetingController.java
+++ src/main/java/br/com/joaotozzi/estudandospringboot/controller/GreetingController.java
@@ -1,26 +1,29 @@
 package br.com.joaotozzi.estudandospringboot.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.joaotozzi.estudandospringboot.config.Greeting;
+
+
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-	@Value("${greeting-name: Mirage}")
-	private String name;
+
+	private final Greeting greeting;
 	
-	@Value("${greeting-coffee: ${greeting-name} is drinking Café Ganador}")
-	private String coffee;
+	public GreetingController(Greeting greeting){
+		this.greeting = greeting;
+	}
 	
 	@GetMapping
 	String getGreeting() {
-		return name;
+		return greeting.getName();
 	}
 	
 	@GetMapping("/coffee")
 	String getCoffe() {
-		return coffee;
+		return greeting.getCoffee();
 	}
 }
