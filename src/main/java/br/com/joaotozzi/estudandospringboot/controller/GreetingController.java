a90c6a6a10060d81d0e6f4aee13560909b54c14d
João Tozzi <joaotozzi@gmail.com>
2022-10-22 13:42:52 -0300

    Usando @Value

---
+++ src/main/java/br/com/joaotozzi/estudandospringboot/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package br.com.joaotozzi.estudandospringboot.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+	@Value("${greeting-name: Mirage}")
+	private String name;
+	
+	@Value("${greeting-coffee: ${greeting-name} is drinking Café Ganador}")
+	private String coffee;
+	
+	@GetMapping
+	String getGreeting() {
+		return name;
+	}
+	
+	@GetMapping("/coffee")
+	String getCoffe() {
+		return coffee;
+	}
+}
